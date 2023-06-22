c23fa2947e9c799e7f0608e89c30bc9a22a1b448
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-06-23 02:21:14 +0530

    Update siteMetaData.js
    shortend metadata

--- src/data/siteMetaData.js
+++ src/data/siteMetaData.js
@@ -1,83 +1,10 @@
 export default {
-    title: 'My Site',
-    description: 'This is my site',
-    url: 'https://mysite.com',
-    image: 'https://mysite.com/images/og-image.jpg',
-    twitter: '@mysite',
+  title: 'My Site',
+  description: 'This is my site',
+  url: 'https://mysite.com',
+  image: 'https://mysite.com/images/og-image.jpg',
+  twitter: '@mysite',
+  facebook: 'mysite',
 
-    // Optional
-
-    // Google Analytics Tracking ID
-    googleAnalyticsId: 'UA-XXXXXX-X',
-
-    // Google Tag Manager Container ID
-    googleTagManagerId: 'GTM-XXXXXX',
-
-    // Facebook Pixel ID
-    facebookPixelId: 'XXXXXXXXXXXXXXX',
-
-    // Twitter Handle
-    twitterHandle: '@mysite',
-
-    // Twitter Site
-    twitterSite: '@mysite',
-
-    // Optional: if you want to include Twitter Card data
-    twitterCardType: 'summary_large_image',
-
-    // Optional: if you want to include Twitter App Card data
-    twitterAppCardType: 'app',
-
-    // Optional: if you want to include Apple Touch Icon meta data
-    appleTouchIcon: '/images/apple-touch-icon.png',
-
-    // Optional: if you want to include a favicon meta data
-    favicon: '/images/favicon.png',
-
-    // Optional: if you want to include a favicon meta data
-    favicon16: '/images/favicon-16x16.png',
-
-    // Optional: if you want to include a favicon meta data
-    favicon32: '/images/favicon-32x32.png',
-
-    // Optional: if you want to include a favicon meta data
-    favicon96: '/images/favicon-96x96.png',
-
-    // Optional: if you want to include a favicon meta data
-    favicon192: '/images/favicon-192x192.png',
-
-    // Optional: if you want to include a favicon meta data
-    favicon512: '/images/favicon-512x512.png',
-
-    // Optional: if you want to include a favicon meta data
-    faviconPrecomposed: '/images/favicon-precomposed.png',
-
-    // Optional: if you want to include a favicon meta data
-    faviconManifest: '/images/manifest.json',
-
-    // Optional: if you want to include a favicon meta data
-    faviconMaskIcon: '/images/safari-pinned-tab.svg',
-
-    // Optional: if you want to include a favicon meta data
-    faviconMsTileImage: '/images/msapplication-icon-144x144.png',
-
-    // Optional: if you want to include a favicon meta data
-    faviconMsTileColor: '#000000',
-
-    // Optional: if you want to include a favicon meta data
-    faviconThemeColor: '#000000',
-
-    // Optional: if you want to include a favicon meta data
-    faviconAppleMobileWebAppCapable: 'yes',
-
-    // Optional: if you want to include a favicon meta data
-    faviconAppleMobileWebAppStatusBarStyle: 'black',
-
-    // Optional: if you want to include a favicon meta data
-    faviconAppleMobileWebAppTitle: 'My Site',
-
-    // Optional: if you want to include a favicon meta data
-    faviconMaskIconColor: '#000000',
-
-    
+  homePageBackgroundImage: '/src/assets/images/home-bg.png'
 }
