037a1803d90fcf104a10880caaa70177ee4611e5
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-06-23 02:21:22 +0530

    Update HomeView.vue
    started homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,30 @@
 <template>
-  <div>
-    <h1>Home View</h1>
-  </div>
+  <section class="flex flex-col px-4">
+    <div
+      class="bg-no-repeat bg-center h-64 rounded-lg"
+      :style="{ backgroundImage: `url(${siteMetaData.homePageBackgroundImage})` }"
+    >
+      <div class="flex flex-col items-center justify-center h-full">
+        <h1 class="text-4xl font-bold text-white">{{ siteMetaData.title }}</h1>
+        <h2 class="text-2xl font-bold text-white">{{ siteMetaData.description }}</h2>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import siteMetaData from '@/data/siteMetaData.js'
 export default {
-
+  name: 'HomeView',
+  data() {
+    return {
+      siteMetaData
+    }
+  },
+  methods: {
+    // ...
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
