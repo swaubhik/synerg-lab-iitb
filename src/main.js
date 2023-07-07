a8cfb87c100d0e3f4497e46efbb0e216ccfc8cf1
Swaubhik Chakraborty <b18csel299@cit.ac.in>
2023-07-07 06:34:44 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
+const backend = import.meta.env.VITE_BACKEND_URL
+
+axios.defaults.baseURL = backend
+axios.defaults.headers = {
+  'ngrok-skip-browser-warning': '69420'
+}
 const app = createApp(App)
 
 app.use(createPinia())
